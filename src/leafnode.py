c6950fb87631f0f895a01eec4ba52160353982cf
jjscout <evggalperin@gmail.com>
2026-01-16 12:39:57 +0200

    add leafnode

---
+++ src/leafnode.py
@@ -0,0 +1,24 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(
+            tag=tag,
+            value=value,
+            children=None,
+            props=props,
+        )
+
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Leaf node must have a value")
+        if not self.tag:
+            return self.value
+        if not self.props:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -5,8 +5,30 @@ from leafnode import LeafNode
 class TestLeafNode(unittest.TestCase):
     def test_to_html(self):
         with self.subTest("no value"):
-            node = LeafNode()
+            node = LeafNode(tag=None, value=None)
             with self.assertRaises(ValueError):
                 node.to_html()
         with self.subTest("no tag"):
-            pass
+            node = LeafNode(tag=None, value="content")
+            self.assertEqual(node.to_html(), "content")
+        with self.subTest("no props"):
+            node = LeafNode(tag="p", value="content")
+            self.assertEqual(node.to_html(), "<p>content</p>")
+        with self.subTest("props"):
+            node = LeafNode(
+                tag="a",
+                value="content",
+                props={"href": "bla.com"},
+            )
+            self.assertEqual(node.to_html(), '<a href="bla.com">content</a>')
+
+    def test_repr(self):
+        node = LeafNode(
+            tag="a",
+            value="content",
+            props={"href": "bla.com"},
+        ) 
+        self.assertEqual(
+            str(node),
+            "LeafNode(a, content, {'href': 'bla.com'})"
+        )
