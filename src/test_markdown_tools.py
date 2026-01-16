9298854a2684ec8d80c292a3093b93dee2d34019
jjscout <evggalperin@gmail.com>
2026-01-16 19:46:47 +0200

    rudimentary markdown tools

---
+++ src/test_markdown_tools.py
@@ -0,0 +1,27 @@
+import unittest
+from markdown_tools import (
+    extract_markdown_images,
+    extract_markdown_links,
+)
+
+
+class TestMarkdownTools(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+        matches = extract_markdown_images(
+            "This is text with an [link](https://i.imgur.com)"
+        )
+        self.assertListEqual(matches, [])
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://i.imgur.com)"
+        )
+        self.assertListEqual([("link", "https://i.imgur.com")], matches)
+        matches = extract_markdown_links(
+            "This is text with an ![img](https://i.imgur.com/lie.png)"
+        )
+        self.assertListEqual([], matches)
