986d57eb8429c57c99c970a8f52efc81bd56830c
jjscout <evggalperin@gmail.com>
2026-01-17 19:28:46 +0200

    determine block type

--- src/markdown_tools.py
+++ src/markdown_tools.py
@@ -1,12 +1,45 @@
 import re
 from typing import List, Tuple
+from enum import Enum
+
+
+class Blocktype(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 
 def extract_markdown_images(text: str) -> List[Tuple[str, str]]:
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
-def extract_markdown_links(text: str) ->  List[Tuple[str, str]]:
+
+def extract_markdown_links(text: str) -> List[Tuple[str, str]]:
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
 
 def markdown_to_blocks(markdown: str) -> List[str]:
     return [b.strip() for b in markdown.split("\n\n") if b != ""]
+
+
+def block_to_block_type(block: str) -> Blocktype:
+    lines = block.split("\n")
+    if block.startswith(tuple("#" * (i + 1) + " " for i in range(6))):
+        return Blocktype.HEADING
+    if block.startswith("```\n") and block.endswith("```") and len(lines) > 1:
+        return Blocktype.CODE
+    if block.startswith("> "):
+        if any(not line.startswith("> ") for line in lines):
+            return Blocktype.PARAGRAPH
+        return Blocktype.QUOTE
+    if block.startswith("- "):
+        if any(not line.startswith("- ") for line in lines):
+            return Blocktype.PARAGRAPH
+        return Blocktype.UNORDERED_LIST
+    if block.startswith("1. "):
+        if any(not line.startswith(f"{i + 1}. ") for i, line in enumerate(lines)):
+            return Blocktype.PARAGRAPH
+        return Blocktype.ORDERED_LIST
+    return Blocktype.PARAGRAPH
--- src/test_markdown_tools.py
+++ src/test_markdown_tools.py
@@ -3,6 +3,8 @@ from markdown_tools import (
     extract_markdown_images,
     extract_markdown_links,
     markdown_to_blocks,
+    block_to_block_type,
+    Blocktype,
 )
 
 
@@ -69,6 +71,75 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        with self.subTest("heading 1"):
+            self.assertEqual(block_to_block_type("# heading1"), Blocktype.HEADING)
+        with self.subTest("heading 2"):
+            self.assertEqual(block_to_block_type("## heading1"), Blocktype.HEADING)
+        with self.subTest("heading 3"):
+            self.assertEqual(block_to_block_type("### heading1"), Blocktype.HEADING)
+        with self.subTest("heading 4"):
+            self.assertEqual(block_to_block_type("#### heading1"), Blocktype.HEADING)
+        with self.subTest("heading 5"):
+            self.assertEqual(block_to_block_type("##### heading1"), Blocktype.HEADING)
+        with self.subTest("heading 6"):
+            self.assertEqual(block_to_block_type("###### heading1"), Blocktype.HEADING)
+        with self.subTest("heading 7 invalid"):
+            self.assertNotEqual(
+                block_to_block_type("######## heading1"), Blocktype.HEADING
+            )
+
+        with self.subTest("code"):
+            self.assertEqual(
+                block_to_block_type("```\n\n```"),
+                Blocktype.CODE,
+            )
+        with self.subTest("not valid code"):
+            self.assertNotEqual(
+                block_to_block_type("``````"),
+                Blocktype.CODE,
+            )
+            self.assertNotEqual(
+                block_to_block_type("```\n``"),
+                Blocktype.CODE,
+            )
+        with self.subTest("quote"):
+            self.assertEqual(
+                block_to_block_type("> quote\n> quote"),
+                Blocktype.QUOTE,
+            )
+        with self.subTest("not quote"):
+            self.assertEqual(
+                block_to_block_type("> quote\n- quote"), Blocktype.PARAGRAPH
+            )
+        with self.subTest("unordered list item"):
+            self.assertEqual(
+                block_to_block_type("- quote\n- quote3"),
+                Blocktype.UNORDERED_LIST,
+            )
+        with self.subTest("not unordered list"):
+            self.assertEqual(
+                block_to_block_type("- quote\n-quote3"),
+                Blocktype.PARAGRAPH,
+            )
+            self.assertEqual(
+                block_to_block_type("- quote\n. quote"), Blocktype.PARAGRAPH
+            )
+        with self.subTest("ordered list item"):
+            self.assertEqual(
+                block_to_block_type("1. quote\n2. q\n3. qq"), Blocktype.ORDERED_LIST
+            )
+        with self.subTest("not ordered list out of order"):
+            self.assertEqual(
+                block_to_block_type("1. quote\n2. q\n4. qq"), Blocktype.PARAGRAPH
+            )
+        with self.subTest("not ordered list item"):
+            self.assertEqual(
+                block_to_block_type("1. quote\n2. q\n> qq"), Blocktype.PARAGRAPH
+            )
+        with self.subTest("paragraph"):
+            self.assertEqual(block_to_block_type("quote"), Blocktype.PARAGRAPH)
 
 
 if __name__ == "__main__":
