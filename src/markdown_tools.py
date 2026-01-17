81848d5d1952e11056fd8df96596d89f192f38f9
jjscout <evggalperin@gmail.com>
2026-01-17 18:56:02 +0200

    splitting blocks

--- src/markdown_tools.py
+++ src/markdown_tools.py
@@ -8,3 +8,5 @@ def extract_markdown_images(text: str) -> List[Tuple[str, str]]:
 def extract_markdown_links(text: str) ->  List[Tuple[str, str]]:
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
+def markdown_to_blocks(markdown: str) -> List[str]:
+    return [b.strip() for b in markdown.split("\n\n") if b != ""]
--- src/test_markdown_tools.py
+++ src/test_markdown_tools.py
@@ -2,6 +2,7 @@ import unittest
 from markdown_tools import (
     extract_markdown_images,
     extract_markdown_links,
+    markdown_to_blocks,
 )
 
 
@@ -25,3 +26,50 @@ class TestMarkdownTools(unittest.TestCase):
             "This is text with an ![img](https://i.imgur.com/lie.png)"
         )
         self.assertListEqual([], matches)
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
